e02d74d1dbf5e504c0168cc3928ba1087425db05
bumblebroo <otis.gustafsson@hotmail.com>
2026-01-21 15:45:47 +0100

    basic plane generation

--- Assets/Terrain.cs
+++ Assets/Terrain.cs
@@ -5,6 +5,25 @@ public class Terrain
 {
     public Mesh Regenerate()
     {
-        return new Mesh();
+        Mesh mesh = new Mesh();
+
+        Vector3[] verticies = new Vector3[4];
+        verticies[0] = new Vector3(-1, 0, -1);
+        verticies[1] = new Vector3(-1, 0, 1);
+        verticies[2] = new Vector3(1, 0, 1);
+        verticies[3] = new Vector3(1, 0, -1);
+
+        int[] triangles = new int[6];
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+        triangles[3] = 0;
+        triangles[4] = 2;
+        triangles[5] = 3;
+        
+        mesh.SetVertices(verticies);
+        mesh.SetIndices(triangles, MeshTopology.Triangles, 0);
+        
+        return mesh;
     }
 }
--- Assets/TerrainScript.cs
+++ Assets/TerrainScript.cs
@@ -6,16 +6,16 @@ public class TerrainScript : MonoBehaviour
 {
     private Terrain terrain;
     
+        public void Regenerate()
+        {
+            if (terrain == null) terrain = new Terrain();
     
-    public void Regenerate()
-    {
-        if (terrain == null) terrain = new Terrain();
+            Mesh mesh = terrain.Regenerate();
+            mesh.name = "TerrainMesh";
+            GetComponent<MeshFilter>().mesh = mesh;
+        }
+        
 
-        Mesh mesh = terrain.Regenerate();
-        mesh.name = "TerrainMesh";
-        GetComponent<MeshFilter>().mesh = mesh;
-    }
-    
     
     void Start()
     {
